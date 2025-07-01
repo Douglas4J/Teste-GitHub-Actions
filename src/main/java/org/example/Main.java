78b4e796640e7d2e17c86124a1cbebc69df428b5
Douglas4J <douglas.ferrerira@onpagtech.com.br>
2025-07-01 14:09:16 -0300

    Testando erro de compilação

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Teste CI na branch branch-teste-pr-ci!");
+        System.out.println("Mensagem sem ponto e vírgula")
     }
 }
