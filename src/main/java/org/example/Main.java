5493da7adfdd050e2a7876e5c3eaf548ba3d1735
Douglas4J <douglas.ferrerira@onpagtech.com.br>
2025-07-01 15:44:11 -0300

    Concertando erro de compilação

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testando Notificação do GitHub Actions no Teams"
+        System.out.println("Testando Notificação do GitHub Actions no Teams");
     }
 }
